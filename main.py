530c346f17eb3ba43a4a3381e0257b3e097b8b38
Calum Dingwall <caburum@users.noreply.github.com>
2025-02-05 15:27:09 -0600

    more tweaks

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.utils import ImageReader
 from streamlit_pdf_viewer import pdf_viewer
 
-def process_pdf(input_pdf_bytes, page_callback):
+def process_pdf(input_pdf_bytes, dpi, page_callback):
 	pdf_document = fitz.open(stream=input_pdf_bytes, filetype="pdf")
 
 	output_buffer = io.BytesIO()
@@ -24,9 +24,10 @@ def process_pdf(input_pdf_bytes, page_callback):
 		page_callback(page=True)
 		page_width, page_height = page.rect.width, page.rect.height
 
-		zoom = 2
-		mat = fitz.Matrix(zoom, zoom)
-		pix = page.get_pixmap(matrix=mat)
+		# zoom = 2
+		# mat = fitz.Matrix(zoom, zoom)
+		# pix = page.get_pixmap(matrix=mat)
+		pix = page.get_pixmap(dpi=dpi)
 		image = np.frombuffer(pix.samples, dtype=np.uint8)
 		# image = image.reshape((pix.height, pix.width, 3))
 
@@ -64,7 +65,11 @@ def process_pdf(input_pdf_bytes, page_callback):
 		_, buffer = cv2.imencode('.png', image, [cv2.IMWRITE_PNG_COMPRESSION, 9])
 		image_bytes = io.BytesIO(buffer.tobytes())
 
-		# todo: limit page width to not be too big (and maybe make configurable)
+		# A4 size: 595 x 842 pt
+		MAX_WIDTH = 842
+		if page_width > MAX_WIDTH:
+			page_height = MAX_WIDTH * page_height / page_width
+			page_width = MAX_WIDTH
 
 		c.setPageSize((page_width, page_height))
 		img_reader = ImageReader(image_bytes)
@@ -76,11 +81,9 @@ def process_pdf(input_pdf_bytes, page_callback):
 
 	return output_buffer
 
-st.set_page_config(page_title="PDF Dark Mode Converter 🌗", page_icon="🌗")
-st.markdown('<div style="text-align: center;font-size:300%;margin-bottom: 30px"><b>PDF Dark Mode Converter 🌗</b></div>', unsafe_allow_html=True)
+st.set_page_config(page_title="PDF Inverter 🌗", page_icon="🌗")
+st.markdown('<div style="text-align: center;font-size:300%;margin-bottom: 30px"><b>PDF Inverter 🌗</b></div>', unsafe_allow_html=True)
 
-# Customization Section
-st.markdown("<hr>", unsafe_allow_html=True)
 def start_processing():
 	st.session_state.start_processing = True
 	st.session_state.processed_files = []
@@ -91,7 +94,10 @@ if 'processed_files' not in st.session_state:
 if 'start_processing' not in st.session_state:
 	st.session_state.start_processing = False
 
+st.markdown("<hr>", unsafe_allow_html=True)
 uploaded_files = st.file_uploader("Upload PDF files", type=["pdf"], accept_multiple_files=True)
+dpi = st.slider("Output Quality (DPI)", 100, 800, 150, step=50)
+st.markdown("<hr>", unsafe_allow_html=True)
 
 if uploaded_files:
 	st.markdown('<div style="text-align: center;font-size:170%;margin-bottom: 10px"><b>🛠 Process PDFs</b></div>', unsafe_allow_html=True)
@@ -105,7 +111,7 @@ if uploaded_files:
 		file_counter = st.empty()
 		total_progress = st.progress(0)
 
-		# todo: make this prettier
+		# todo: make this prettier, add subprogress (extracting, inverting, etc.)
 		def page_callback(page=None, total=None):
 			global total_pages, completed_pages
 			if total is not None:
@@ -118,7 +124,6 @@ if uploaded_files:
 			file_counter.markdown(
 				f"<div style='text-align: center; font-size: 24px; margin-bottom: 20px;'>"
 				f"<b>Processing file {i+1} of {total_files}</b><br>",
-				# f"<span>{(i+1) * 100 / total_files:.2f}% completed</span></div>",
 				unsafe_allow_html=True
 			)
 			with st.spinner(f"Processing {uploaded_file.name}"):
@@ -126,7 +131,7 @@ if uploaded_files:
 				input_pdf_stream = io.BytesIO(binary_pdf)
 				input_pdf_stream.seek(0)
 
-				output_pdf_stream = process_pdf(input_pdf_stream.getvalue(), page_callback)
+				output_pdf_stream = process_pdf(input_pdf_stream.getvalue(), dpi, page_callback)
 
 				output_stream = output_pdf_stream.getvalue()
 				st.session_state.processed_files.append(
@@ -143,7 +148,7 @@ if uploaded_files:
 		st.markdown(f'<div style="text-align: center;font-size:170%;margin-bottom: 10px"><b>🔎 View PDF</b></div>', unsafe_allow_html=True)
 		col1, col2 = st.columns([3, 1])
 		with col1:
-			st.write(f"➔ {j + 1} -  {file_name}   ✅")
+			st.write(f"➔ {file_name} ✅ ({len(output_stream) / 1024:,.2f} KB)")
 		with col2:
 			st.download_button(
 				label="Download",
